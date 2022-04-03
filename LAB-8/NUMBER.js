372909ff1866d5cb9fc0621338d58e33f39dd4ae
20IT127 <82073450+Hutishseju@users.noreply.github.com>
2022-04-03 17:38:45 +0530

    Delete NUMBER.js

--- LAB-8/NUMBER.js
+++
@@ -1,8 +0,0 @@
-let variable = prompt("Enter N");
-let res = "";
-let x = document.getElementById("num");
-for (let i = 0; i <= variable; i++) {
-    res += i;
-    res += ' ';
-}
-x.textContent = res;
